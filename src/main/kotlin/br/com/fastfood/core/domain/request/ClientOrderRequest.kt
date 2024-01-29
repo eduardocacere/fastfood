d2a3f8fa3d7f8cab3d7d43961f35c53a22d570f5
Sanches <sanches@Sanchess-iMac.local>
2024-01-29 11:47:59 -0300

    Incluindo descrição de objetos

--- src/main/kotlin/br/com/fastfood/core/domain/request/ClientOrderRequest.kt
+++ src/main/kotlin/br/com/fastfood/core/domain/request/ClientOrderRequest.kt
@@ -1,7 +1,32 @@
 package br.com.fastfood.core.domain.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(
+        description = "Model for a client."
+)
 data class ClientOrderRequest(
+        @field:Schema(
+                title = "Name of client",
+                description = "Name of client",
+                example = "Peter Parker",
+                type = "String"
+        )
         val name: String? = null,
+
+        @field:Schema(
+                title = "Email of client",
+                description = "Email of client",
+                example = "teste@teste.com",
+                type = "String"
+        )
         val email: String? = null,
+
+        @field:Schema(
+                title = "Cpf of client" ,
+                description = "No punctuation ",
+                example = "11122233344",
+                type = "String"
+        )
         val cpf: String? = null
 )
