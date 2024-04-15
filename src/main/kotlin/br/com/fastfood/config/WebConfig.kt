63ed0b04fefc1c5c33e085427c7716a2b9e329cf
Sanches <sanches@Sanchess-iMac.local>
2024-04-14 23:53:36 -0300

    Adicionando config

---
+++ src/main/kotlin/br/com/fastfood/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package br.com.fastfood.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig: WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("resources/static/");
+    }
+}
